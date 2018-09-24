f0cf9e9ab7fdf89af1b61e12d958e1db96978a18
mat2uken <ku@yasagure.jp>
2018-09-24 15:56:04 +0900

    update

--- update_unound_adblocking_conf.sh
+++ update_unound_adblocking_conf.sh
@@ -2,6 +2,6 @@
 
 cd /etc/unbound
 cat blocking.conf.head > blocking.conf
-/usr/bin/python3 gen_adblocker.py >> blocking.conf
+/usr/bin/python3 gen_adblocker.py 192.168.99.1 >> blocking.conf
 /etc/init.d/unbound restart
 
