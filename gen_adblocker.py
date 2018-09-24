67e5abe8f52f2d9d9b069821f5d8334f686bbf59
mat2uken <ku@yasagure.jp>
2018-09-24 15:01:47 +0900

    Update gen_adblocker.py
    refactoring

--- gen_adblocker.py
+++ gen_adblocker.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import sys
+dns_server = "192.168.99.1"
+if len(sys.argv) == 2:
+    dns_server = sys.argv[1]
+
+local_zone_entry = 'local-zone: "{}." redirect'
+local_data_entry = 'local-data: "{}. IN A {}"'.format("{}", dns_server)
+
 import urllib.request
-
-url = 'https://280blocker.net/files/280blocker_host.txt'
 headers = { "User-Agent" :  "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)" }
+def get_hosts_from_url(url):
+    req = urllib.request.Request(url, None, headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode("utf-8-sig")
 
-req = urllib.request.Request(url, None, headers)
-with urllib.request.urlopen(req) as res:
-    body = res.read().decode("utf-8-sig")
+    lines = []
+    for line in body.split('\n'):
+        line = line.strip()
+        if line == '' or line.startswith("#"):
+            continue
+        lines.append(line)
 
-#blocking_conf_file = open("blocking.conf", "wb")
+    return list(set(lines))
 
-local_zone_entry = 'local-zone: "{}." static'
-for line in body.split('\r\n'):
-    line = line.strip()
-    if line == '' or line.startswith("#"):
-        continue
-#    blocking_conf_file.write(local_zone_entry.format(line).encode() + b"\n")
-    print(local_zone_entry.format(line))
+def gen_unbound_conf_lies(hosts):
+    conf = []
+    hosts = get_hosts_from_url(url)
+    for host in hosts:
+        conf.append(local_zone_entry.format(host))
+        conf.append(local_data_entry.format(host))
+    return '\n'.join(conf)
+
+urls = ('https://280blocker.net/files/280blocker_host.txt',
+        'https://raw.githubusercontent.com/anudeepND/youtubeadsblacklist/master/domainlist.txt')
+for url in urls:
+    print(gen_unbound_conf_lies(url))
+
