56370c7e67cae9584d555c403a6ddb8423ce0508
mat2uken <ku@yasagure.jp>
2019-08-29 16:02:51 +0900

    misc

--- gen_adblocker.py
+++ gen_adblocker.py
@@ -2,13 +2,14 @@
 # coding: utf-8
 
 import sys
-dns_server = "127.0.0.1"
+#dns_server = "127.0.0.1"
+dns_server = "0.0.0.0"
 if len(sys.argv) == 2:
     dns_server = sys.argv[1]
 
-local_zone_entry = 'local-zone: "{}." static'
+#local_zone_entry = 'local-zone: "{}." static'
 #local_zone_entry = 'local-zone: "{}." redirect'
-#local_data_entry = 'local-data: "{}. IN A {}"'.format("{}", dns_server)
+local_data_entry = 'local-data: "{}. IN A {}"'.format("{}", dns_server)
 
 import urllib.request
 headers = { "User-Agent" :  "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)" }
@@ -22,6 +23,8 @@ def get_hosts_from_url(url):
         line = line.strip()
         if line == '' or line.startswith("#"):
             continue
+
+
         lines.append(line)
 
     return list(set(lines))
@@ -30,11 +33,12 @@ def gen_unbound_conf_lies(hosts):
     conf = []
     hosts = get_hosts_from_url(url)
     for host in hosts:
-        conf.append(local_zone_entry.format(host))
-#        conf.append(local_data_entry.format(host))
+#        conf.append(local_zone_entry.format(host))
+        conf.append(local_data_entry.format(host))
     return '\n'.join(conf)
 
-urls = ('https://280blocker.net/files/280blocker_host.txt',
+urls = (
+        'https://280blocker.net/files/280blocker_host.txt',
         'https://raw.githubusercontent.com/anudeepND/youtubeadsblacklist/master/domainlist.txt')
 for url in urls:
     print(gen_unbound_conf_lies(url))
--- update_unound_adblocking_conf.sh
+++ update_unound_adblocking_conf.sh
@@ -4,8 +4,16 @@
 
 cd /etc/unbound
 cat blocking.conf.head > blocking.conf
-/usr/bin/python3 gen_adblocker.py 127.0.0.1 >> blocking.conf
-curl https://warui.intaa.net/adhosts/local_data.conf.txt | sed -e 's/IN A 0.0.0.0/IN A 127.0.0.1/g' > warui_intaa_blocking.conf
+#/usr/bin/python3 gen_adblocker.py 127.0.0.1 >> blocking.conf
+#curl https://warui.intaa.net/adhosts/local_data.conf.txt | sed -e 's/IN A 0.0.0.0/IN A 127.0.0.1/g' > warui_intaa_blocking.conf
 
+/usr/bin/python3 gen_adblocker.py 0.0.0.0 >> blocking.conf
+curl https://warui.intaa.net/adhosts/local_data.conf.txt > warui_intaa_blocking.conf
+
+sed -i -e '/s.click.aliexpress/d' warui_intaa_blocking.conf
+sed -i -e '/analytics.twitter.com/d' warui_intaa_blocking.conf
+
+mkdir -p /tmp/www
+cp blocking.conf warui_intaa_blocking.conf /tmp/www/
 /etc/init.d/unbound restart
 
